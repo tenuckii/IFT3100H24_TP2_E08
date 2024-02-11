002c94d60d5384dba0ca8770f7ddd3beaa08e0ca
tenuckii <152208465+tenuckii@users.noreply.github.com>
2024-02-11 17:58:11 -0500

    wip

---
+++ obj/msys2/Release/src/ofApp.d
@@ -0,0 +1,192 @@
+obj/msys2/Release/src/ofApp.o: \
+ E:/of_v0.12.0_msys2_mingw64_release/apps/myApps/IFT3100H24_TP2_E08/src/ofApp.cpp \
+ E:/of_v0.12.0_msys2_mingw64_release/apps/myApps/IFT3100H24_TP2_E08/src/ofApp.h \
+ ../../../libs/openFrameworks/ofMain.h \
+ ../../../libs/openFrameworks/utils/ofConstants.h \
+ ../../../libs/tess2/include/tesselator.h \
+ ../../../libs/openFrameworks/utils/ofFileUtils.h \
+ ../../../libs/openFrameworks/utils/ofLog.h \
+ ../../../libs/openFrameworks/utils/ofUtils.h \
+ ../../../libs/openFrameworks/utils/ofRandomEngine.h \
+ ../../../libs/openFrameworks/utils/ofSingleton.hpp \
+ ../../../libs/openFrameworks/math/ofMath.h \
+ ../../../libs/openFrameworks/utils/ofRandomDistributions.h \
+ ../../../libs/openFrameworks/utils/ofSystemUtils.h \
+ ../../../libs/openFrameworks/utils/ofURLFileLoader.h \
+ ../../../libs/openFrameworks/events/ofEvents.h \
+ ../../../libs/openFrameworks/events/ofEventUtils.h \
+ ../../../libs/openFrameworks/events/ofEvent.h \
+ ../../../libs/openFrameworks/utils/ofTimer.h \
+ ../../../libs/openFrameworks/utils/ofFpsCounter.h \
+ ../../../libs/openFrameworks/utils/ofThread.h \
+ ../../../libs/openFrameworks/utils/ofThreadChannel.h \
+ ../../../libs/openFrameworks/utils/ofJson.h \
+ ../../../libs/openFrameworks/types/ofParameter.h \
+ ../../../libs/openFrameworks/types/ofPoint.h \
+ ../../../libs/openFrameworks/math/ofVec3f.h \
+ ../../../libs/openFrameworks/math/ofVec2f.h \
+ ../../../libs/openFrameworks/math/ofMathConstants.h \
+ ../../../libs/openFrameworks/math/ofVec4f.h \
+ ../../../libs/openFrameworks/types/ofRectangle.h \
+ ../../../libs/openFrameworks/types/ofColor.h \
+ ../../../libs/openFrameworks/utils/ofXml.h \
+ ../../../libs/openFrameworks/graphics/ofGraphicsBaseTypes.h \
+ ../../../libs/openFrameworks/graphics/ofGraphicsConstants.h \
+ ../../../libs/openFrameworks/types/ofTypes.h \
+ ../../../libs/openFrameworks/types/ofParameterGroup.h \
+ ../../../libs/openFrameworks/math/ofVectorMath.h \
+ ../../../libs/openFrameworks/math/ofMatrix3x3.h \
+ ../../../libs/openFrameworks/math/ofMatrix4x4.h \
+ ../../../libs/openFrameworks/math/ofQuaternion.h \
+ ../../../libs/openFrameworks/communication/ofSerial.h \
+ ../../../libs/openFrameworks/communication/ofArduino.h \
+ ../../../libs/openFrameworks/gl/ofFbo.h \
+ ../../../libs/openFrameworks/gl/ofTexture.h \
+ ../../../libs/openFrameworks/gl/ofGLBaseTypes.h \
+ ../../../libs/openFrameworks/gl/ofGLRenderer.h \
+ ../../../libs/openFrameworks/graphics/ofPolyline.h \
+ ../../../libs/openFrameworks/graphics/ofPolyline.inl \
+ ../../../libs/openFrameworks/app/ofAppRunner.h \
+ ../../../libs/openFrameworks/app/ofWindowSettings.h \
+ ../../../libs/openFrameworks/app/ofMainLoop.h \
+ ../../../libs/openFrameworks/graphics/of3dGraphics.h \
+ ../../../libs/openFrameworks/3d/of3dPrimitives.h \
+ ../../../libs/openFrameworks/3d/ofMesh.h \
+ ../../../libs/openFrameworks/gl/ofGLUtils.h \
+ ../../../libs/openFrameworks/3d/ofMesh.inl \
+ ../../../libs/openFrameworks/3d/ofNode.h \
+ ../../../libs/openFrameworks/graphics/ofBitmapFont.h \
+ ../../../libs/openFrameworks/graphics/ofPixels.h \
+ ../../../libs/openFrameworks/graphics/ofGraphics.h \
+ ../../../libs/openFrameworks/utils/ofMatrixStack.h \
+ ../../../libs/openFrameworks/graphics/ofPath.h \
+ ../../../libs/openFrameworks/graphics/ofPolyline.h \
+ ../../../libs/openFrameworks/gl/ofVboMesh.h \
+ ../../../libs/openFrameworks/3d/ofMesh.h \
+ ../../../libs/openFrameworks/gl/ofVbo.h \
+ ../../../libs/openFrameworks/gl/ofBufferObject.h \
+ ../../../libs/openFrameworks/graphics/ofTessellator.h \
+ ../../../libs/openFrameworks/gl/ofLight.h \
+ ../../../libs/openFrameworks/gl/ofShadow.h \
+ ../../../libs/openFrameworks/gl/ofShader.h \
+ ../../../libs/openFrameworks/gl/ofMaterial.h \
+ ../../../libs/openFrameworks/gl/ofMaterialBaseTypes.h \
+ ../../../libs/openFrameworks/gl/ofCubeMap.h \
+ ../../../libs/openFrameworks/graphics/ofCairoRenderer.h \
+ C:/msys/mingw64/include/cairo/cairo.h \
+ C:/msys/mingw64/include/cairo/cairo-version.h \
+ C:/msys/mingw64/include/cairo/cairo-features.h \
+ C:/msys/mingw64/include/cairo/cairo-deprecated.h \
+ ../../../libs/openFrameworks/graphics/ofGraphicsCairo.h \
+ ../../../libs/openFrameworks/graphics/ofImage.h \
+ ../../../libs/openFrameworks/graphics/ofRendererCollection.h \
+ ../../../libs/openFrameworks/graphics/ofTrueTypeFont.h \
+ ../../../libs/openFrameworks/app/ofBaseApp.h \
+ ../../../libs/openFrameworks/sound/ofSoundBaseTypes.h \
+ ../../../libs/openFrameworks/app/ofAppBaseWindow.h \
+ ../../../libs/openFrameworks/app/ofAppGLFWWindow.h \
+ ../../../libs/openFrameworks/app/ofAppGlutWindow.h \
+ ../../../libs/openFrameworks/sound/ofSoundStream.h \
+ ../../../libs/openFrameworks/sound/ofSoundPlayer.h \
+ ../../../libs/openFrameworks/sound/ofSoundBuffer.h \
+ ../../../libs/openFrameworks/video/ofVideoGrabber.h \
+ ../../../libs/openFrameworks/video/ofVideoBaseTypes.h \
+ ../../../libs/openFrameworks/video/ofVideoPlayer.h \
+ ../../../libs/openFrameworks/3d/of3dUtils.h \
+ ../../../libs/openFrameworks/3d/ofCamera.h \
+ ../../../libs/openFrameworks/3d/ofEasyCam.h
+E:/of_v0.12.0_msys2_mingw64_release/apps/myApps/IFT3100H24_TP2_E08/src/ofApp.h:
+../../../libs/openFrameworks/ofMain.h:
+../../../libs/openFrameworks/utils/ofConstants.h:
+../../../libs/tess2/include/tesselator.h:
+../../../libs/openFrameworks/utils/ofFileUtils.h:
+../../../libs/openFrameworks/utils/ofLog.h:
+../../../libs/openFrameworks/utils/ofUtils.h:
+../../../libs/openFrameworks/utils/ofRandomEngine.h:
+../../../libs/openFrameworks/utils/ofSingleton.hpp:
+../../../libs/openFrameworks/math/ofMath.h:
+../../../libs/openFrameworks/utils/ofRandomDistributions.h:
+../../../libs/openFrameworks/utils/ofSystemUtils.h:
+../../../libs/openFrameworks/utils/ofURLFileLoader.h:
+../../../libs/openFrameworks/events/ofEvents.h:
+../../../libs/openFrameworks/events/ofEventUtils.h:
+../../../libs/openFrameworks/events/ofEvent.h:
+../../../libs/openFrameworks/utils/ofTimer.h:
+../../../libs/openFrameworks/utils/ofFpsCounter.h:
+../../../libs/openFrameworks/utils/ofThread.h:
+../../../libs/openFrameworks/utils/ofThreadChannel.h:
+../../../libs/openFrameworks/utils/ofJson.h:
+../../../libs/openFrameworks/types/ofParameter.h:
+../../../libs/openFrameworks/types/ofPoint.h:
+../../../libs/openFrameworks/math/ofVec3f.h:
+../../../libs/openFrameworks/math/ofVec2f.h:
+../../../libs/openFrameworks/math/ofMathConstants.h:
+../../../libs/openFrameworks/math/ofVec4f.h:
+../../../libs/openFrameworks/types/ofRectangle.h:
+../../../libs/openFrameworks/types/ofColor.h:
+../../../libs/openFrameworks/utils/ofXml.h:
+../../../libs/openFrameworks/graphics/ofGraphicsBaseTypes.h:
+../../../libs/openFrameworks/graphics/ofGraphicsConstants.h:
+../../../libs/openFrameworks/types/ofTypes.h:
+../../../libs/openFrameworks/types/ofParameterGroup.h:
+../../../libs/openFrameworks/math/ofVectorMath.h:
+../../../libs/openFrameworks/math/ofMatrix3x3.h:
+../../../libs/openFrameworks/math/ofMatrix4x4.h:
+../../../libs/openFrameworks/math/ofQuaternion.h:
+../../../libs/openFrameworks/communication/ofSerial.h:
+../../../libs/openFrameworks/communication/ofArduino.h:
+../../../libs/openFrameworks/gl/ofFbo.h:
+../../../libs/openFrameworks/gl/ofTexture.h:
+../../../libs/openFrameworks/gl/ofGLBaseTypes.h:
+../../../libs/openFrameworks/gl/ofGLRenderer.h:
+../../../libs/openFrameworks/graphics/ofPolyline.h:
+../../../libs/openFrameworks/graphics/ofPolyline.inl:
+../../../libs/openFrameworks/app/ofAppRunner.h:
+../../../libs/openFrameworks/app/ofWindowSettings.h:
+../../../libs/openFrameworks/app/ofMainLoop.h:
+../../../libs/openFrameworks/graphics/of3dGraphics.h:
+../../../libs/openFrameworks/3d/of3dPrimitives.h:
+../../../libs/openFrameworks/3d/ofMesh.h:
+../../../libs/openFrameworks/gl/ofGLUtils.h:
+../../../libs/openFrameworks/3d/ofMesh.inl:
+../../../libs/openFrameworks/3d/ofNode.h:
+../../../libs/openFrameworks/graphics/ofBitmapFont.h:
+../../../libs/openFrameworks/graphics/ofPixels.h:
+../../../libs/openFrameworks/graphics/ofGraphics.h:
+../../../libs/openFrameworks/utils/ofMatrixStack.h:
+../../../libs/openFrameworks/graphics/ofPath.h:
+../../../libs/openFrameworks/graphics/ofPolyline.h:
+../../../libs/openFrameworks/gl/ofVboMesh.h:
+../../../libs/openFrameworks/3d/ofMesh.h:
+../../../libs/openFrameworks/gl/ofVbo.h:
+../../../libs/openFrameworks/gl/ofBufferObject.h:
+../../../libs/openFrameworks/graphics/ofTessellator.h:
+../../../libs/openFrameworks/gl/ofLight.h:
+../../../libs/openFrameworks/gl/ofShadow.h:
+../../../libs/openFrameworks/gl/ofShader.h:
+../../../libs/openFrameworks/gl/ofMaterial.h:
+../../../libs/openFrameworks/gl/ofMaterialBaseTypes.h:
+../../../libs/openFrameworks/gl/ofCubeMap.h:
+../../../libs/openFrameworks/graphics/ofCairoRenderer.h:
+C:/msys/mingw64/include/cairo/cairo.h:
+C:/msys/mingw64/include/cairo/cairo-version.h:
+C:/msys/mingw64/include/cairo/cairo-features.h:
+C:/msys/mingw64/include/cairo/cairo-deprecated.h:
+../../../libs/openFrameworks/graphics/ofGraphicsCairo.h:
+../../../libs/openFrameworks/graphics/ofImage.h:
+../../../libs/openFrameworks/graphics/ofRendererCollection.h:
+../../../libs/openFrameworks/graphics/ofTrueTypeFont.h:
+../../../libs/openFrameworks/app/ofBaseApp.h:
+../../../libs/openFrameworks/sound/ofSoundBaseTypes.h:
+../../../libs/openFrameworks/app/ofAppBaseWindow.h:
+../../../libs/openFrameworks/app/ofAppGLFWWindow.h:
+../../../libs/openFrameworks/app/ofAppGlutWindow.h:
+../../../libs/openFrameworks/sound/ofSoundStream.h:
+../../../libs/openFrameworks/sound/ofSoundPlayer.h:
+../../../libs/openFrameworks/sound/ofSoundBuffer.h:
+../../../libs/openFrameworks/video/ofVideoGrabber.h:
+../../../libs/openFrameworks/video/ofVideoBaseTypes.h:
+../../../libs/openFrameworks/video/ofVideoPlayer.h:
+../../../libs/openFrameworks/3d/of3dUtils.h:
+../../../libs/openFrameworks/3d/ofCamera.h:
+../../../libs/openFrameworks/3d/ofEasyCam.h:
